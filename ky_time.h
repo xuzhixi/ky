f23be7f4a44ccabe35bf7c975a56923761649590
xuzhixi <932834199@163.com>
2012-09-19 15:24:07 +0800

    ky_library 0.1.2.1
    1. 在 ky_malloc.h 和 ky_time.h 添加了注释

--- ky_time.h
+++ ky_time.h
@@ -5,7 +5,19 @@
 extern "C" {
 #endif
 
+/**
+ * @brief 获取当前的日期和时间
+ *
+ * @param  dtStr	保存日期时间的缓存区(要大于等于20个字节)
+ * @return			返回缓存区的首地址
+ */
 extern char *ky_date_time(char *dtStr);
+/**
+ * @brief 获取当前的时间
+ *
+ * @param  dtStr	保存时间的缓存区(要大于等于9个字节)
+ * @return			返回缓存区的首地址
+ */
 extern char *ky_now_time(char *tStr);
 
 #ifdef _CPLUSPLUS
